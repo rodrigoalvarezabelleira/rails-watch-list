cca7213b25a158bd293e507089bb4f788860cd53
Rodrigo Alvarez Abelleira <rodrigo.alvarez.abelleira@gmail.com>
2022-11-17 16:21:45 +0100

    Backend almost finished

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,7 +19,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to lists_path
+    redirect_to list_path
   end
 
   private
