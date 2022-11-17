5825be26efc948e478840a40e50aad5f163490a3
Rodrigo Alvarez Abelleira <rodrigo.alvarez.abelleira@gmail.com>
2022-11-17 16:20:28 +0100

    Backend almost done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
